248b724c67ec91e082541eeba3e93ed6bb6ccebd
Senon Silvestre <42272961+senjaymac@users.noreply.github.com>
2022-07-04 07:39:11 +0800

    submit can now save data inputs
    contact form datas are now stored in txt

--- VB - Contact Tracing App/VB - Contact Tracing App/Form1.vb
+++ VB - Contact Tracing App/VB - Contact Tracing App/Form1.vb
@@ -1,16 +1,19 @@
 ﻿Public Class Form1
     Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
         Dim form As System.IO.StreamWriter
-        form = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\Element 54\Documents\Repos\VB - Contact Tracing\VB - Contact Tracing App\ContactTracingForm.Txt", True)
+        form = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\Element 54\Documents\Repos\VB - Contact Tracing\VB - Contact Tracing App\Contact TracingForm.Txt", True)
         form.WriteLine("Contact Tracing Form")
         form.WriteLine("")
         form.WriteLine("Date: " + pickerDate.Text)
         form.WriteLine("Time: " + DateTime.Now.ToString("h:mm:ss tt"))
+        form.WriteLine("Personal Informations")
+        form.WriteLine("First Name: " + txtboxFirstName.Text)
+        form.WriteLine("Last Name: " + txtbxLastName.Text)
 
 
 
 
-
+        form.Close()
     End Sub
 
     Private Sub time(sender As Object, e As EventArgs) Handles timerClock.Tick
